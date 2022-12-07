895e92bbf4efd0c7b5cae46e5b4855cee8412b00
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2022-12-07 16:23:09 +0100

    Save changes for level && blueprints

--- Assets/_Project/Scripts/CoinTrigger.cs
+++ Assets/_Project/Scripts/CoinTrigger.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains;
+using MoreMountains.Feedbacks;
 
 public class CoinTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject pickFeedback;
+    [SerializeField] private MMFeedbacks soundFeedback;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.transform.tag == "Player" || other.transform.tag == "Companion")
         {
             EventsSystem.current.CoinCollected();
+            Instantiate(pickFeedback, transform.position, transform.rotation);
+            soundFeedback.PlayFeedbacks();
             Destroy(gameObject);
         }
     }
