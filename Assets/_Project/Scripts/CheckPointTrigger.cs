ebc604e95e0fec9da3e1db92189935bba35f3f93
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2023-03-14 23:25:17 +0100

    Changed Level-1 && created new checkpoint cheat

--- Assets/_Project/Scripts/CheckPointTrigger.cs
+++ Assets/_Project/Scripts/CheckPointTrigger.cs
@@ -9,6 +9,8 @@ public class CheckPointTrigger : MonoBehaviour
     [SerializeField] private bool deactivateAfterUse = false;
     [SerializeField] private bool startActivated = false;
 
+    private bool isActive = false;
+
     private void Start()
     {
         if(CheckPointSystem == null)
@@ -21,15 +23,39 @@ public class CheckPointTrigger : MonoBehaviour
             CheckPointSystem.SetSpawnPointPos(spawnTrans);
         }
 
+        isActive = startActivated;
+
+        EventsSystem.current.onCheckPointActivated += OnCheckPointActivated;
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Player")
+        if (other.transform.tag == "Player" && !isActive)
         {
             CheckPointSystem.SetSpawnPointPos(spawnTrans);
             if(deactivateAfterUse)
                 GetComponent<BoxCollider>().enabled = false;
+
+            EventsSystem.current.CheckPointActivated();
+            isActive = true;
+
         }
     }
+    
+    private void OnCheckPointActivated()
+    {
+        if(isActive)
+        {
+            isActive = false;
+            Debug.Log(this.gameObject.name);
+        }
+            
+    }
+
+    private void OnDestroy()
+    {
+        EventsSystem.current.onCheckPointActivated -= OnCheckPointActivated;
+    }
+
 }
