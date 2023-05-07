5906f572797455f1c05e7299bf3fbd890cc37240
Marc Ramis <marc.ramis@enti.cat>
2023-05-07 18:38:25 +0200

    hit sound effect + reference singleton

--- Assets/_Project/Scripts/Feedback/WeaponFeedbackController.cs
+++ Assets/_Project/Scripts/Feedback/WeaponFeedbackController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class WeaponFeedbackController : FeedbackController
 {
+    
+    [SerializeField] private AudioClip hitSoundSmall;
+    [SerializeField] private AudioClip hitSoundBig;
+    [SerializeField] private AudioClip hitSoundBig2;
+    
+    [Space]
     [SerializeField] private TrailRenderer trail1;
     [SerializeField] private TrailRenderer trail2;
     [SerializeField] private TrailRenderer trail3;
     [SerializeField] private TrailRenderer trail4;
 
+    private bool hitOnce = true;
+    private float hitCd;
 
     /////////// MAKE DAMAGE
     public void PlayAttack()
@@ -25,5 +33,49 @@ public class WeaponFeedbackController : FeedbackController
         trail2.emitting = false;
         //trail3.emitting = false;
         //trail4.emitting = false;
+    }
+
+    /////////// HIT IMPACT
+    public void PlayHitImpact(int counter)
+    {
+        if (hitOnce)
+        {
+            hitOnce = false;
+            Invoke(nameof(ResetHitOnce), 0.5f);
+
+            switch ((counter))
+            {
+                case 0:
+
+                    PlaySoundEffect(hitSoundSmall);
+
+                    break;
+                case 1:
+
+                    PlaySoundEffect(hitSoundSmall);
+
+                    break;
+                case 2:
+                    
+                    int randomCount = Random.Range(1, 2);
+                    switch ((randomCount))
+                    {
+                        case 1:
+
+                            PlaySoundEffect(hitSoundBig);
+                            break;
+
+                        case 2:
+                            PlaySoundEffect(hitSoundBig2);
+                            break;
+                    };
+                    break;
+            };
+        }
+    }
+
+    private void ResetHitOnce()
+    {
+        hitOnce = true;
     }
 }
