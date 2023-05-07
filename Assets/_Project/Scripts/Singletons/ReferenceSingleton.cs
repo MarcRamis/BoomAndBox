6cb65531c6a429d5781e65aa35d555556339577c
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2023-05-07 20:00:41 +0200

    Save changes with singleton class

--- Assets/_Project/Scripts/Singletons/ReferenceSingleton.cs
+++ Assets/_Project/Scripts/Singletons/ReferenceSingleton.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReferenceSingleton : SingletonMonobehaviour<ReferenceSingleton>
-{
+{ 
     public GameObject player;
     public Player playerScript;
     public GameObject companion;
     public Companion companionScript;
+
+    public new void Awake()
+    {
+        if (instance == null)
+        {
+            //instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
