16f33bf43d9175d7cf685d5eeaaf8d8eb854d88c
Alex Alcaide Arroyes <alex.alcaide@enti.cat>
2023-05-09 14:18:04 +0200

    Fixed logic for navigation and submit in the menu

--- Assets/_Project/Scripts/Menu/MenuLogic.cs
+++ Assets/_Project/Scripts/Menu/MenuLogic.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
@@ -9,29 +10,120 @@ public class MenuLogic : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private string sceneNameToChangeOnPlay = "Level1";
+    [SerializeField] private float timeToWait = 0.5f;
 
-    [Header("Objects")]
+    [Header("Play Menu")]
+    [SerializeField] private GameObject playMenu = null;
+    [SerializeField] private GameObject playButton;
+
+    [Header("Options")]
     [SerializeField] private GameObject options = null;
-
-    [Header("Button to options")]
     [SerializeField] private GameObject optionSlider;
+    [SerializeField] private AudioMixer audioMixer;
+    [Header("Credits")]
+    [SerializeField] private GameObject credits = null;
+    [SerializeField] private GameObject returnButton = null;
 
+    private float timer = 0.0f;
+    private bool wait = true;
+
+    private void Awake()
+    {
+        wait = true;
+        timer = 0.0f;
+    }
+
+    private void Update()
+    {
+        if(wait)
+        {
+            timer += Time.deltaTime;
+
+            if(timer > timeToWait ) 
+            {
+                wait = false;
+                timer = 0.0f;
+            }
+
+        }
+    }
+
+    public void StartWait()
+    {
+        wait = true;
+    }
+
+    //-------MENU-------//
     public void Play()
     {
-        SceneManager.LoadScene(sceneNameToChangeOnPlay);
+        if(!wait)
+        {
+            SceneManager.LoadScene(sceneNameToChangeOnPlay);
+            StartWait();
+        }
+            
     }
 
     public void Options()
     {
-        var eventSystem = EventSystem.current;
-        eventSystem.SetSelectedGameObject(optionSlider, new BaseEventData(eventSystem));
+        if(!wait)
+        {
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(optionSlider, new BaseEventData(eventSystem));
 
-        options.SetActive(true);
+            options.SetActive(true);
+            StartWait();
+        }
+        
     }
 
     public void Exit()
     {
-        Application.Quit();
+        if(!wait)
+        {
+            Application.Quit();
+        }
+            
     }
 
+    public void Credits()
+    {
+        if (!wait)
+        {
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(returnButton, new BaseEventData(eventSystem));
+            credits.SetActive(true);
+            StartWait();
+        }
+            
+    }
+
+    //-------OPTIONS-------//
+    public void MasterMusicControl(float _sliderValue)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(_sliderValue) * 20.0f);
+    }
+
+    public void CloseOptions()
+    {
+        if (!wait)
+        {
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(playButton, new BaseEventData(eventSystem));
+            options.SetActive(false);
+            StartWait();
+        }
+    }
+
+    //-------CREDITS-------//
+    public void CloseCredits()
+    {
+        if(!wait)
+        {
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(playButton, new BaseEventData(eventSystem));
+            credits.SetActive(false);
+            StartWait();
+        }
+    }
 }
